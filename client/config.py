71615cd9af750b7d70f905b0f6f48321ecba7843
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2024-11-28 22:49:37 +0500

    api url added to config

--- client/config.py
+++ client/config.py
@@ -5,11 +5,13 @@ from environs import Env
 @dataclass()
 class Config:
     bot_token: str
+    api_url: str
 
-def get_config():
+def get_config() -> Config:
     env = Env()
     env.read_env()
 
     return Config(
-        bot_token=env("BOT_TOKEN")
+        bot_token=env("BOT_TOKEN"),
+        api_url=env("API_URL")
     )

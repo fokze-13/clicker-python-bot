1cd21fde3ae8e8256d83a626fd45d66563181a62
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2024-11-28 22:50:12 +0500

    new user and get clicks in telegram interface added

--- client/handlers/callback_handlers.py
+++ client/handlers/callback_handlers.py
@@ -3,12 +3,21 @@ from aiogram import Router
 from aiogram import F
 from client.keyboards.client_inline_keyboards import start_keyboard_button, clicker_keyboard
 from client.lexicons import lexicon_ru
+import requests
+from client.config import Config, get_config
+
+config: Config = get_config()
 
 router: Router = Router()
 
 @router.callback_query(F.data == start_keyboard_button.callback_data)
 async def start_callback(callback: CallbackQuery):
     await callback.message.delete()
-    await callback.message.answer(lexicon_ru.clicker_message,
-                                  reply_markup=clicker_keyboard)
+    response = requests.get(
+        f"{config.api_url}/clicks/get?user_id={callback.from_user.id}"
+    )
+
+    if response.status_code == 200:
+        await callback.message.answer(lexicon_ru.clicker_message.format(clicks=response.json()),
+                                        reply_markup=clicker_keyboard)
 
--- client/handlers/client_handlers.py
+++ client/handlers/client_handlers.py
@@ -3,9 +3,19 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from client.lexicons import lexicon_ru
 from client.keyboards import client_inline_keyboards
+import requests
+from client.config import Config, get_config
+import json
+
+config: Config = get_config()
 
 router: Router = Router()
 
+
 @router.message(CommandStart())
 async def start(message: Message):
-    await message.answer(lexicon_ru.start_message, reply_markup=client_inline_keyboards.start_keyboard)
+    response = requests.post(
+        f"{config.api_url}/user/new?user_id={message.from_user.id}")
+
+    if response.status_code == 200:
+        await message.answer(lexicon_ru.start_message, reply_markup=client_inline_keyboards.start_keyboard)
